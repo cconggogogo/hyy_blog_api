61507a57ff38e1dee5d8b4a5e89c0738b632724d
admin <15623552283@163.com>
2019-05-10 18:36:18 +0800

    修改github登陆相关权限

--- app.config.js
+++ app.config.js
@@ -59,14 +59,14 @@ exports.GITHUB = {
 
 	// 请把生产环境的 redirect_url，client_id 和 client_secret 中的 "****", 换成自己创建的 OAuth App 的具体参数即可。
 	// // 生产环境
-  // redirect_url: 'http://biaochenxuying.cn/login',
-  // client_id: '*****',
-	// client_secret: '*****',
+    redirect_uri: 'http://www.huyangyang.cn:81/login',
+    client_id: 'e28cdff4af8aae4b5750',
+    client_secret: 'ec98cf755627fdc59755abc1802865234a471709',
 
 	// 开发环境 （参数可以直接用，公供测试）
-	redirect_url: "http://localhost:3001/login",
-  client_id: "502176cec65773057a9e",
-	client_secret: "65d444de381a026301a2c7cffb6952b9a86ac235",
+	// redirect_url: "http://localhost:3001/login",
+  // client_id: "502176cec65773057a9e",
+	// client_secret: "65d444de381a026301a2c7cffb6952b9a86ac235",
 	
 };
 
